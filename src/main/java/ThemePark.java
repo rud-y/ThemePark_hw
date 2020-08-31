ffd4d61025190607d63b58322cd3abfbb8729fce
rudy <rudolf.zachar@gmail.com>
2020-08-31 19:33:35 +0100

    Interfaces created: ISecurity, IReviewed, ITicketed; implemented in Playground and TobaccoStall

---
+++ src/main/java/ThemePark.java
@@ -0,0 +1,15 @@
+import attractions.Attraction;
+import behaviours.IReviewed;
+import behaviours.ITicketed;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
+public class ThemePark {
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+    private ArrayList<IReviewed> allReviews;
+    private ArrayList<ITicketed> allTickets;
+
+
+ }
