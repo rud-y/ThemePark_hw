baf6ef8823bf5c9e405e20abe12dd7930194af54
rudy <rudolf.zachar@gmail.com>
2020-08-31 23:18:28 +0100

    Dodgems implement ITicketed interface, and test chargeHalfPriceUnder12Yrs passing

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,19 +1,24 @@
 package attractions;
 
+import behaviours.ITicketed;
 import people.Visitor;
 
-public class Dodgems extends Attraction {
+public class Dodgems extends Attraction implements ITicketed {
 
     public Dodgems(String name, int rating) {
 
         super(name, rating);
     }
 
-//    public double defaultPrice() {
-//        return 4.50;
-//    }
-//    public double priceFor(Visitor visitor) {
-//        return
-//    }
+    public double defaultPrice() {
+        return 4.50;
+    }
+    public double priceFor(Visitor visitor) {
+        if(visitor.getAge() < 12) {
+            return this.defaultPrice() / 2;
+        }else{
+            return defaultPrice();
+        }
+    }
 
 }
